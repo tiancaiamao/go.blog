2dc00aac65b349bdfe4830c485bf213bca9207a3
Arthur <tiancaiamao@gmail.com>
2015-08-08 21:15:51 +0800

    博客index页做好了

--- main.scm
+++ main.scm
@@ -1,6 +1,9 @@
-(use spiffy lowdown sxml-transforms intarweb uri-common files)
+(use spiffy lowdown sxml-transforms intarweb uri-common files medea)
 
 (load "template/root.scm")
+
+(define project-path "/Users/genius/project/src/github.com/tiancaiamao/go.blog/")
+(define content-path (string-append project-path "content"))
 
 (define (md-handler filename)
   (with-input-from-file (string-append (root-path) filename)
@@ -10,6 +13,28 @@
 			     (lambda () 
 			       (SXML->HTML (page "title" (cons content '()) '() '()))))))
 		 (send-response body: body))))))
+
+(define (index->sxml data)
+  (define (item field x)
+    (cdr (assq field x)))
+  (define fn
+    (lambda (x)
+      `(p (@ (class "blogtitle"))
+	  (a (@ (href ,(item 'File x)))
+	     ,(item 'Title x))
+	  (br (span (@ (class "date")) (item 'Date x))))))
+  (map fn data))
+
+(define (blog-handler)
+  (with-input-from-file (string-append content-path "/out.json")
+    (lambda ()
+      (let* ((data (read-json))
+	     (bloglist (index->sxml (vector->list data)))
+	     (content (list '(h1 (@ (class "title")) "Article index") bloglist)))
+	(let ((body (with-output-to-string 
+		      (lambda () 
+			(SXML->HTML (page "title" content '() '()))))))
+	  (send-response body: body))))))
 
 (define router
   (lambda (continue)
@@ -21,7 +46,7 @@
 	  (let ((p (car pl)))
 	    (cond
 	     ((string=? p "") 1)
-	     ((string=? p "index") 2)
+	     ((string=? p "index") (blog-handler))
 	     ((string=? p "about") 3)
 	     ((string=? p "category") 4)
 	     ((string=? p "tags") 5)
@@ -29,7 +54,7 @@
 	     ((string-suffix-ci? ".md" p)
 	      (parameterize ((file-extension-handlers 
 			      `(("md" . ,md-handler)))
-			     (root-path "/Users/genius/project/src/github.com/tiancaiamao/go.blog/content"))
+			     (root-path content-path))
 			    (continue)))
 	     (else
 	      ((handle-not-found) path))))
