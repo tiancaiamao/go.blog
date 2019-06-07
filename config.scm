88638bee42e962080b1ffe6ea9b881d774eee23a
tiancaiamao <tiancaiamao@gmail.com>
2019-06-08 02:09:16 +0800

    clean up the old chicken implementation

--- config.scm
+++
@@ -1,4 +0,0 @@
-(define project-path "/media/genius/OS/project/go.blog/")
-(define content-path (string-append project-path "content"))
-(define template-path (string-append project-path "template"))
-(define PORT 8088)
